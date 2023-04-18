47434847e3d5ddae06d8611489df6b6853165929
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2023-04-18 13:14:59 +0800

    First commit

---
+++ hostedpage.php
@@ -0,0 +1,58 @@
+<?php
+
+require 'lib/subscription.php';
+
+if ( !isset( $_GET['hostedpage'] ) ) {
+    throw new Exception( 'Hosted page data is required' );
+}
+
+$auth_json = file_get_contents('auth.json');
+$auth = json_decode($auth_json, true);
+
+$apiKey = $auth['api_key'];
+$apiSecret = $auth['api_secret'];
+
+$hostedpage = $_GET['hostedpage'];
+$subscription = new Subscription($apiKey, $apiSecret);
+//Get hosted page details
+try {
+    $api_data = $subscription->hostedPage($hostedpage);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
+$_subscription = $api_data->subscription;
+
+if (!property_exists($api_data, "invoice")) {
+	//If the subscription is trial, activate it and redirect to thank you page
+	if ($_subscription->trial_days > 0) {
+		//Do here your additional things if require.
+		//Activate the trial subscription
+		$subscription->activateTrialSubscription($_subscription->id);
+		//Redirect to the thank you page
+		$subscription->redirectThankYou($api_data->subscription->id, $api_data->subscription->customer_id, $api_data->product->redirect_url);
+	}
+}
+
+$user = $api_data->user;
+$customer = $api_data->customer;
+$product = $api_data->product;
+$plan = $api_data->plan;
+$invoice = $api_data->invoice;
+$currency = $user->currency;
+
+//Do your payment processor task here
+//After complete the payment process you have to record the payment for the invoice due. Use the following example for that:
+
+try {
+    $invoice_id = $invoice->id;
+    $payment_mode = "CHIP";
+    $transaction_data = "ABC"; //string/object
+    $payment_note = ""; //Note for your payment transaction if any
+    $api_data = $subscription->recordPayment($invoice_id, $payment_mode, $payment_note, $transaction_data);
+
+//Redirct to thank you page
+    $subscription->redirectThankYou($api_data->subscription->id, $api_data->subscription->customer_id, $api_data->product->redirect_url);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
---
+++ lib/subscription.php
@@ -0,0 +1,118 @@
+<?php
+
+class Subscription {
+
+  var $apiKey = "";
+  var $apiSecret = "";
+  var $apiUrl = "https://payments.pabbly.com/api/v1/";
+  var $thankyouUrl = "https://payments.pabbly.com/thankyou/";
+  public function __construct($apiKey, $apiSecret) {
+    if (!$apiKey && !$apiSecret) {
+      throw new Exception('Error: apikey and api secret are required');
+    }
+
+    $this->apiKey = $apiKey;
+    $this->apiSecret = $apiSecret;
+  }
+
+  public function apiPath($path) {
+    return $this->apiUrl . $path;
+  }
+
+  public function subscribe($data) {
+
+    $post_data = array(
+      'first_name' => $data['first_name'],
+      'last_name' => $data['last_name'],
+      'email' => $data['email'],
+      'gateway_type' => $data['gateway_name'],
+      'street' => $data['street'],
+      'city' => $data['city'],
+      'state' => $data['state'],
+      'zip_code' => $data['zip_code'],
+      'country' => $data['country'],
+      'plan_id' => $data['plan_id'],
+    );
+
+    return $this->curl( $this->apiPath('subscription'), $post_data );
+  }
+
+  public function recordPayment($invoice_id, $payment_mode, $payment_note, $transaction_data) {
+    if (!$invoice_id) {
+      throw new Exception('Error: invoice id is required');
+    }
+
+    $post_data = array(
+      'payment_mode' => $payment_mode,
+      'payment_note' => $payment_note,
+      'transaction' => $transaction_data
+    );
+
+    return $this->curl( $this->apiPath('invoice/recordpayment/'), $post_data );
+  }
+
+  public function redirectThankYou($subscriptionId, $customerId, $redirect_url = '') {
+    if (isset($_GET['hostedpage'])) {
+      $hostedpage = $_GET['hostedpage'];
+    } else if (isset($_POST['hostedpage'])) {
+      $hostedpage = $_POST['hostedpage'];
+    } else {
+      $hostedpage = '';
+    }
+
+    if ($redirect_url && $redirect_url !== '') {
+      $redirect_url = $redirect_url . "?hostedpage=" . $hostedpage;    
+    } else {
+      $redirect_url = $this->thankyouUrl . $subscriptionId . "/" . $customerId;    
+    }
+
+    header('Location:' . $redirect_url); 
+    exit;
+  }
+
+  public function hostedPage($hostedpage) {
+    $post_data = array(
+      'hostedpage' => $hostedpage
+    );
+
+    return $this->curl( $this->apiPath('hostedpage'), $post_data );
+  }
+
+  /**
+   * Activate the trial subscription
+   */
+  public function activateTrialSubscription($subscription_id) {
+    if (!$subscription_id) {
+      throw new Exception('Error: invoice id is required');
+    }
+
+    return $this->curl( $this->apiPath('subscription/activatetrial/' . $subscription_id) );
+  }
+
+  public function getCustomer($customerId)
+  {
+    return $this->curl( $this->apiPath('customer/' . $customerId) );
+  }
+
+  private function curl( $api_path, $post_data = array() ) {
+    $process = curl_init( $api_path );
+
+    curl_setopt($process, CURLOPT_USERPWD, $this->apiKey . ':' . $this->apiSecret );
+    curl_setopt($process, CURLOPT_TIMEOUT, 30);
+    curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+
+    if ( !empty($post_data) ) {
+      curl_setopt($process, CURLOPT_POSTFIELDS, $post_data );
+    }
+
+    $return = curl_exec($process);
+
+    if ($return === false) {
+      throw new Exception('Error: ' . curl_error($process));
+    }
+
+    curl_close($process);
+
+    return $return;
+  }
+}
